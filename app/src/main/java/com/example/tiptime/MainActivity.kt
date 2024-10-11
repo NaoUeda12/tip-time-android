54b49e0a2119ca322c5155c3a14a6bfd0f2badb9
NaoUeda12 <ueda.nao@chrono.co.jp>
2024-10-11 18:29:56 +0900

    ボタンタップでトーストの表示

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.tiptime
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +13,22 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+//        setContentView(R.layout.activity_main)
+        // bindingの初期化とsetContentViewを行う
+        binding = ActivityInputDataBinding.inflate(layoutInflater)
+            .apply { setContentView(this.root) }
+
+        var ServiceCost = binding.editTextnumber.toString()
+
+        findViewById<Button>(R.id.calculate_button)
+            .setOnClickListener {
+                Toast.makeText(applicationContext, "ボタンが押されました", Toast.LENGTH_SHORT).show()
+            }
+
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
         }
     }
-}
