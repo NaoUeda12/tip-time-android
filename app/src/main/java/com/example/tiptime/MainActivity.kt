07506b3aa4ce9a7b63b4e76bb2679ef70b8d469c
NaoUeda12 <ueda.nao@chrono.co.jp>
2024-10-15 15:14:46 +0900

    チップ計算結果の表示

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -9,26 +9,53 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.tiptime.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         // bindingの初期化とsetContentViewを行う
-        binding = ActivityInputDataBinding.inflate(layoutInflater)
-            .apply { setContentView(this.root) }
 
-        var ServiceCost = binding.editTextnumber.toString()
 
-        findViewById<Button>(R.id.calculate_button)
-            .setOnClickListener {
-                Toast.makeText(applicationContext, "ボタンが押されました", Toast.LENGTH_SHORT).show()
+        binding.calculateButton.setOnClickListener {
+//                Toast.makeText(applicationContext, "ボタンが押されました", Toast.LENGTH_SHORT)
+//                    .show()
+            // EditTextの値を取得
+            val costInput = binding.costOfService.text.toString()
+
+            // Radio Buttonの値を取得
+            val tipPercentageID = binding.tipOptions.checkedRadioButtonId
+            var tipPercentage = 0.0
+
+            when (tipPercentageID) {
+                R.id.option_twenty_percent -> tipPercentage = 0.20
+                R.id.option_eighteen_percent -> tipPercentage =0.18
+                R.id.option_fifteen_percent -> tipPercentage =0.15
+            }
+            var tip = 0.0
+            var totalAmount = 0.0
+
+            if (costInput.isNotEmpty()) {
+                var cost = costInput.toDouble()
+                tip = cost * tipPercentage
+                totalAmount = cost + tip
             }
 
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-//            insets
-//        }
+            binding.tipResult.text = String.format("tip: $%.2f", tip)
+
+            binding.totalAmount.text = String.format("total Amount: $%.2f", totalAmount)
+
+
+
         }
+
     }
+
+
+}
+
