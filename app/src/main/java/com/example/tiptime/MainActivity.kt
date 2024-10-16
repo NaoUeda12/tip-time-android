c825ea0a16bc7076ade3fc64afaf09ab7e705c24
NaoUeda12 <ueda.nao@chrono.co.jp>
2024-10-16 17:45:48 +0900

    10/16の成果物のPush

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -7,6 +7,7 @@ import android.widget.Button
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.provider.FontsContractCompat.Columns
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.tiptime.databinding.ActivityMainBinding
@@ -14,17 +15,29 @@ import com.example.tiptime.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
+    var showUpCalculateResult = false
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+
+        // bindingの初期化とsetContentViewを行う
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        // bindingの初期化とsetContentViewを行う
+
+        if (!showUpCalculateResult) {
+            binding.tipResult.setVisibility(View.INVISIBLE)
+            binding.totalAmount.setVisibility(View.INVISIBLE)
+        }
+
 
 
         binding.calculateButton.setOnClickListener {
 //            Toast.makeText(applicationContext, "ボタンが押されました", Toast.LENGTH_SHORT)
 //                .show()
+            showUpCalculateResult = true
+
 
             // EditTextの値を取得
             val costInput = binding.costOfService.text.toString()
@@ -33,10 +46,19 @@ class MainActivity : AppCompatActivity() {
             val tipPercentageID = binding.tipOptions.checkedRadioButtonId
             var tipPercentage = 0.0
 
+            // otherの数値の取得
+            var otherNumberID = binding.enterHere.text.toString()
+//            if(otherNumberID)
+
+
+            var otherNumber = otherNumberID.toDoubleOrNull() ?: 0.0
+            var changeToDecimal = otherNumber / 100
+
             when (tipPercentageID) {
                 R.id.option_twenty_percent -> tipPercentage = 0.20
                 R.id.option_eighteen_percent -> tipPercentage = 0.18
                 R.id.option_fifteen_percent -> tipPercentage = 0.15
+                R.id.option_other_percent -> tipPercentage = changeToDecimal
             }
             var tip = 0.0
             var totalAmount = 0.0
@@ -48,19 +70,33 @@ class MainActivity : AppCompatActivity() {
             }
 
             val toggleButton = binding.roundUpSwitch.isChecked
-            // ONのときの処理
-            if (toggleButton) {
-                binding.tipResult.text = String.format("tip: $%d", tip.toInt())
 
-                binding.totalAmount.text =
-                    String.format("total Amount: $%d", totalAmount.toInt())
 
-                // OFFのときの処理
-            } else {
-                binding.tipResult.text = String.format("tip: $%.2f", tip)
+            if (showUpCalculateResult) {
+                binding.tipResult.setVisibility(View.VISIBLE)
+                binding.totalAmount.setVisibility(View.VISIBLE)
 
-                binding.totalAmount.text = String.format("total Amount: $%.2f", totalAmount)
+
+//                val toggleButton = binding.roundUpSwitch.isChecked
+                // ONのときの処理
+                if (toggleButton) {
+                    binding.tipResult.text = String.format("Tip Amount: $%d", tip.toInt())
+
+                    binding.totalAmount.text =
+                        String.format("Total Amount: $%d", totalAmount.toInt())
+
+
+                    // OFFのときの処理
+                } else {
+                    binding.tipResult.text = String.format("Tip Amount: $%.2f", tip)
+
+                    binding.totalAmount.text = String.format("Total Amount: $%.2f", totalAmount)
+                }
+
+
             }
         }
     }
+
+
 }
